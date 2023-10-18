585e4fd1fb73e5442eac3822f70668ee9e0b637c
unknown <mehjabeenhossain01@gmail.com>
2023-10-18 17:08:12 +0600

    adminDashboard

---
+++ thunder-knight-edutech-connect/src/hooks/useClass.jsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+
+const useClass = () => {
+
+    const {data: classes = [], isLoading: loading, refetch} = useQuery({
+        queryKey:['classes'],
+        queryFn: async()=>{
+            const res = await fetch('https://summer-camp-server-lilac.vercel.app/class');
+            return res.json();
+        }
+
+    })
+    return [classes, loading, refetch]
+}
+
+export default useClass;
---
+++ thunder-knight-edutech-connect/src/pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
+
+const ManageUsers = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const {data: users = [], refetch} = useQuery(['users'], async()=>{
+        const res = await axiosSecure.get('/users')
+        return res.data;
+    });
+
+    const handleMakeAdmin = user =>{
+       fetch(`https://summer-camp-server-lilac.vercel.app/users/admin/${user._id}`, {
+            method:'PATCH'
+        })
+        .then(res=> res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
+        })
+    }
+    return (
+        <div className="w-full">
+            <div className="overflow-x-auto">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        users.map((user, index)=> <tr key={user._id}>
+            <th>{index + 1}</th>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.role === 'admin'? 'admin' : <button onClick={()=>handleMakeAdmin(user)} className="btn btn-ghost btn-sm bg-red-500 text-white">user</button>}</td>
+            <td><button className="btn btn-ghost btn-sm bg-red-500 text-white">Delete</button></td>
+          </tr>)
+      }
+    </tbody>
+  </table>
+</div>
+        </div>
+    );
+};
+
+export default ManageUsers;
