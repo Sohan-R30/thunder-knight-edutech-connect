67e5f39aa5780424220ecf93748acd162915d06b
sohan-r30 <sohanakondo139@gmail.com>
2023-10-18 22:32:51 +0600

    merge shohidul arifin branch

---
+++ thunder-knight-edutech-connect-server/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const jwt= require('jsonwebtoken');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const verifyJWT = (req,res,next)=>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error:true, message:'Unauthorized access'});
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+        if(error){
+            return res.status(401).send({error:true, message:'Unauthorized access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.59h68ks.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // jwt
+    app.post('/jwt',(req,res)=>{
+        const user = req.body;
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        res.send({token});
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send('Edutech server is running')
+})
+
+
+app.listen(port,()=>{
+    console.log(`Edutech server is running on port ${port}`);
+})
+
+
